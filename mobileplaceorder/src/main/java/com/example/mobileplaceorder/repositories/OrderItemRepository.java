3f169366d9b662ea23550ae6c6da378a6cb41e2c
Ace <47049298+ramareds18@users.noreply.github.com>
2024-04-18 15:34:00 +0700

    Update OrderItemRepository.java
    condition to empty cart

--- mobileplaceorder/src/main/java/com/example/mobileplaceorder/repositories/OrderItemRepository.java
+++ mobileplaceorder/src/main/java/com/example/mobileplaceorder/repositories/OrderItemRepository.java
@@ -2,10 +2,18 @@ package com.example.mobileplaceorder.repositories;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.mobileplaceorder.entities.OrderItem;
 
 public interface OrderItemRepository extends CrudRepository<OrderItem, Integer>{
 	List<OrderItem> findAll();
+	
+	@Transactional
+	@Modifying
+    @Query(nativeQuery = true, value = "DELETE FROM order_item_tbl WHERE ordernumber IS NULL")
+    int deleteByOrderNumberIsNull();
 }
